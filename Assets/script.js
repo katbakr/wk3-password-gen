a095eccc135e7d6ccc69d147cab9f3eb96e86837
Kat Baker <katrinamourine@gmail.com>
2022-10-17 17:08:19 -0700

    adding randomization from char lists

--- Assets/script.js
+++ Assets/script.js
@@ -13,6 +13,7 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 ///////////////////////// DO NOT CHANGE ABOVE HERE /////////////////////////
 function genPass() {
+    //variables for user input prompts and character lists
     var chooseLength = window.prompt ("How long do you want your password? Enter number between 8-128.")
     var includeUppercase = window.prompt ("Do you need uppercase letters in your password? Enter Yes or No.")
     var includeNumbers = window.prompt ("Do you need numbers in your password? Enter Y or N.")
@@ -22,6 +23,27 @@ function genPass() {
     var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     var numberList = ["1", "2","3", "4", "5", "6", "7", "8", "9", "0"]
     var specList = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "+", "?", "/", "<", ">"]
+    //variable for final password deploy(?)
+    var passwordFinal = []
+    //for loop for pulling random option from array above
+    for (let i = 0; i < chooseLength; i++)
+    {
+        var uppercaseIndex = Math.floor(Math.random()*uppercaseList.length);
+        var lowercaseIndex = Math.floor(Math.random()*lowercaseList.length);
+        var numberIndex = Math.floor(Math.random()*numberList.length);
+        var specialIndex = Math.floor(Math.random()*specList.length);
+    //variable for NEED HELP
+        var uppercase = uppercaseList[uppercaseIndex]
+        var lowercase = lowercaseList[lowercaseIndex]
+        var number = numberList[numberIndex]
+        var specialChar = specList[specialIndex]
 
+    //If statements for using user inputs to choose what is added to final password
+    if (includeUppercase==="Y","y","yes","YES", "Yes") {
+        passwordGroup.push(uppercase)
+       } else if (includeUppercase!=="Y","y","yes","YES", "Yes"){
+        passwordGroup.push()
+       }
 
+}
 }
